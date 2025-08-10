ef4454306b685f1995404ea5cb975ebba77c559c
Khairul <khairul.web1@gmail.com>
2025-08-10 15:28:50 +0600

    banner change

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import slide_1 from '../../assets/slide-1.png';
+import slide_1 from '../../assets/banner-transport.png';
 import slide_2 from '../../assets/slide-2.png';
-import slide_3 from '../../assets/slide-3.png';
+import slide_3 from '../../assets/banner-call-center.png';
 
 const Banner = () => {
   return (
@@ -13,7 +13,7 @@ const Banner = () => {
             src={slide_1}
             className="w-full rounded-2xl" />
           <h3 className='absolute bg-[#ece3e3] py-2 px-3 rounded-lg'>update available</h3>
-          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>flowering trees need water,sunlight,Pruning and Fertilizing</p>
+          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>New model car from HG company.</p>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
@@ -24,7 +24,7 @@ const Banner = () => {
             src={slide_2}
             className="w-full rounded-2xl" />
           <h3 className='absolute bg-[#ece3e3] py-2 px-3 rounded-lg'>update available</h3>
-          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>some essential pineapple plant care tips is watering, Ideal temperature 18-29'C and Fertilizing</p>
+          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>Local pineapple raw market</p>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
@@ -35,7 +35,7 @@ const Banner = () => {
             src={slide_3}
             className="w-full rounded-2xl" />
           <h3 className='absolute bg-[#ece3e3] py-2 px-3 rounded-lg'>update available</h3>
-          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>we need some steps Seed or grafted plant, Watering and soil PH 5.5-5.6</p>
+          <p className='absolute bottom-4 lg:bottom-20 left-2 lg:left-5 text-2xl font-bold lg:text-3xl'>wonderful call center</p>
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
